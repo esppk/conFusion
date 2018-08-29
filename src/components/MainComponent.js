dc0d21d10d5fccb509979f7d0b48673508239a21
Emick L <esppk@outlook.com>
2018-08-30 00:57:34 +0800

    navmenu router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,38 +2,33 @@ import React, { Component } from "react"
 import Menu from "./MenuComp"
 import Header from "./HeaderComp"
 import Footer from "./FooterComp"
+import Home from "./HomeComp"
 import { DISHES } from "../shared/dishes"
 import Dishdetail from "./DishdetailComp"
+import { Switch, Route, Redirect } from "react-router-dom"
 
 class Main extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      dishes: DISHES,
-      selectedDish: null
+      dishes: DISHES
     }
   }
-
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId })
-  }
-
   render() {
+    const HomePage = () => <Home />
     return (
       <div className="App">
         <Header />
-        <Menu
-          dishes={this.state.dishes}
-          onClick={dishId => this.onDishSelect(dishId)}
-        />
-        <Dishdetail
-          selectedDish={
-            this.state.dishes.filter(
-              dish => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     )
